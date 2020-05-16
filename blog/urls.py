88dd84016d597c453a544366b9e3f571cfcc04ce
mochipenguin <yyad567@gmail.com>
2020-05-16 16:58:08 +0900

    Changed the HTML for the site

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    #url(r'^$', views.post_list, name='post_list'),
+    # ex: /polls/
+    path('', views.post_list, name='post_list'),
+]
