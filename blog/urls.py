745c98c5f00cf096691e91be6000dee72f399b73
mochipenguin <yyad567@gmail.com>
2020-06-13 21:46:42 +0900

    Add veiw and template

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     #url(r'^$', views.post_list, name='post_list'),
     # ex: /polls/
     path('', views.post_list, name='post_list'),
+    path('^post/(?p<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
 ]
